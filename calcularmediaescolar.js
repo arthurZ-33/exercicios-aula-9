3c2a40d1a4e69747196b190fdd7de8ea826e89a3
arthur <arthur.rodrigue64@gmail.com>
2025-04-14 21:40:16 -0300

    adicionei o exercicio de media

---
+++ calcularmediaescolar.js
@@ -0,0 +1,13 @@
+let aluno1 = [7,5];
+let aluno2 = [5,8];
+let aluno3 = [6,9];
+
+let mediaAluno1 = (aluno1[0] + aluno1[1])/2;
+let mediaAluno2 = (aluno2[0] + aluno2[1])/2;
+let mediaAluno3 = (aluno3[0] + aluno3[1])/2;
+
+let media =7
+
+console.log(`${mediaAluno1 >= media ? 'Aluno aprovado':'aluno reprovado'}`);
+console.log(`${mediaAluno2 >= media ? 'Aluno aprovado':'aluno reprovado'}`);
+console.log(`${mediaAluno3 >= media ? 'Aluno aprovado':'aluno reprovado'}`);
